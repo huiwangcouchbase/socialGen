b632726e1b97af8bc7ebed84b95390df20291bad
Your Name <you@example.com>
2017-10-10 16:45:00 -0700

    Add initb.sh

--- scripts/initb.sh
+++ scripts/initb.sh
@@ -12,5 +12,24 @@ java -cp "$PWD/target/SocialGen.jar" socialGen.PreDataGenerator -a "$PWD" $USERD
 for i in `cat $PWD/output/partitions`
 do
    PARTITION=`echo $i |cut -d ':' -f2`
-   java -Xms1g -Xmx2g -cp "$PWD/target/SocialGen.jar" socialGen.DataGenerator "$PWD" $PARTITION $@
+   java -Xms1g -Xmx2g -cp "$PWD/target/SocialGen.jar" socialGen.DataGenerator "$PWD" $PARTITION $@ &
+   pids+=($!)
 done
+
+echo "waiting processes"
+
+result=0
+for pid in "${pids[@]}"; do
+   wait "$pid"
+   if [ $? -eq 0 ]
+   then
+      echo "it worked"
+   else
+      echo "it failed"
+      result=1
+   fi
+done
+
+echo "finish waiting processes"
+
+exit $result
